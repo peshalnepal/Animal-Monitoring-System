450179fad02466e783a10ba5845a3ec6c3c8f470
peshalnepal <peshalnepal3@gmail.com>
2019-08-02 19:27:46 +0545

    before final

--- NRF_T/NRF_T/main.c
+++ NRF_T/NRF_T/main.c
@@ -15,19 +15,21 @@ char RWdata(char value_send);
 void initialize(void);
 uint8_t add_value[5];
 void flush_every(void);
-<<<<<<< HEAD
-char animalId[]="iCAT00001";
-=======
-char animalId[]="CAT00001";
-
->>>>>>> c4cac47a7eb1ecc349da7440e02a9c94c31050de
+static char animalId[]="iCAT00001";
+static char tempr_animal[3];
+static char value_of_tempr[2];
+ static char value_of_humidity[2];
+ char location[26];
+ void getlocation(void);
 void setnrf(uint8_t registers,uint8_t values_to_put);
 void transferstatusdata(uint8_t values);
 uint8_t getvalue(uint8_t read_status);
 void send_chunck_of_data(char *,uint8_t);
-	  unsigned char tempr_animal[4];	
-    	unsigned char value_of_tempr[2];
-	    unsigned char value_of_humidity[3];
+void reset(void);
+unsigned char return_char(void);
+int main(void)
+{
+
 	    uint16_t temperature_in_voltage_format;
 	 	uint8_t thelow;
 	 	uint8_t tempr_of_animal;
@@ -38,39 +40,18 @@ void send_chunck_of_data(char *,uint8_t);
 	 	uint8_t checksum=0;
 	 	uint8_t sum=0;
 	 	uint8_t arrange_array=0;
-
-
-void reset(void);
-int main(void)
-{
-
-<<<<<<< HEAD
 	 DDRA&=~(1<<1);
-	 ADCSRA |=(1<<ADPS2)|(1<<ADPS2);//this bit is set when we want to divide CLK frequency by 8
+	 ADCSRA |=(1<<ADPS2);//this bit is set when we want to divide CLK frequency by 8
 	 ADMUX |=(1<<REFS0)|(1<<ADLAR)|(1<<MUX0);//REFSO set ref voltage to VCC and ADLAR is use for left shifting values in ADCH and ADCL register
 	 ADCSRA |=(1<<ADEN); //this is use for enabling ADC
-=======
-	uint8_t temperature=0;
-	uint8_t temperature1=0;
-	uint8_t humidity=0;
-	uint8_t humidity1=0;
-	uint8_t checksum=0;
-	uint8_t sum=0;
-	uint8_t hh=0;
-	 DDRA|=(1<<1)|(1<<2);
->>>>>>> c4cac47a7eb1ecc349da7440e02a9c94c31050de
 	  UBRRH=baud_prescale>>8;
 	  UBRRL=baud_prescale;
 	  UCSRC |=(1<<URSEL)|(3<<UCSZ0);
 	  UCSRB |=(1<<TXEN)|(1<<RXEN);
-<<<<<<< HEAD
-=======
-	
->>>>>>> c4cac47a7eb1ecc349da7440e02a9c94c31050de
 	 DDRB|=(1<<SS)|(1<<CE)|(1<<MOSI)|(1<<SCK);
 	 DDRB&=~(1<<MISO);
     SPCR|=(1<<SPE)|(1<<MSTR);
-	  SPCR&=~(1<<SPIE);
+	SPCR&=~(1<<SPIE);
 	SPCR&=~(1<<DORD);
 	SPCR&=~(1<<CPOL);
 	SPCR&=~(1<<SPR1);
@@ -79,22 +60,13 @@ int main(void)
 	initialize();
     while (1) 
     {
-<<<<<<< HEAD
-	          unsigned  char *contain_both_humidity_tempr;
+	         char *contain_both_humidity_tempr;
+			  arrange_array=0;
 			  _delay_ms(10);
 		      reset();
 		      _delay_ms(10);
 		      send_chunck_of_data(animalId,sizeof(animalId));
 		      _delay_ms(100);
-=======
-		unsigned char value_of_tempr[2];
-        unsigned char value_of_humidity[3];
-		unsigned char *contain_both_humidity_tempr;
-		      reset();
-		      _delay_ms(10);
-		      send_chunck_of_data(animalId,sizeof(animalId));
-		      _delay_ms(150);
->>>>>>> c4cac47a7eb1ecc349da7440e02a9c94c31050de
 		      flush_every();
 		      _delay_ms(10);
 		      DDRC|=(1<<PINC0);
@@ -115,10 +87,9 @@ int main(void)
 		      {
 			      sum=sum-checksum;
 		      }
-		      if(sum>=0 && sum<=5)
+		      if(sum>=0 && sum<=3)
 		      {
 			      itoa(temperature,value_of_tempr,10);
-<<<<<<< HEAD
 				    _delay_ms(10);
 			      contain_both_humidity_tempr[arrange_array]=0x73;
 			      for(uint8_t i=0;i<2;i++)
@@ -130,25 +101,24 @@ int main(void)
 			      _delay_ms(10);
 				  arrange_array++;
 			      contain_both_humidity_tempr[arrange_array]=0x68;
-			      for(uint8_t i=0;i<4;i++)
+			      for(uint8_t i=0;i<3;i++)
 			      {
 					  arrange_array++;
 				      contain_both_humidity_tempr[arrange_array]=value_of_humidity[i];
 			      }
 		      }
-			  
-		      _delay_ms(1400);
+		      _delay_ms(1500);
 			  arrange_array++;
 			  ADCSRA|=(1<<ADSC);
-			 _delay_ms(100);
+			 _delay_ms(20);
 			 ADCSRA&=~(1<<ADSC);
 			  thelow=ADCL;// done so that lag doesn't occur while running as we need to access ADCH and ADCL at same time
 			  temperature_in_voltage_format= (ADCH<<2)|(thelow>>6);//this is use to get 10 bit data from ADC
 			  tempr_of_animal=temperature_in_voltage_format/2;
-			  itoa(tempr_of_animal,tempr_animal,10);
-			  _delay_ms(20);
+			  itoa(temperature_in_voltage_format,tempr_animal,10);
+			  _delay_ms(10);
 			   contain_both_humidity_tempr[arrange_array]=0x74;
-			  for (uint8_t i=0;i<4;i++)
+			  for (uint8_t i=0;i<3;i++)
 			  {
 				   arrange_array++;
 				   contain_both_humidity_tempr[arrange_array]=tempr_animal[i];
@@ -159,31 +129,17 @@ int main(void)
 		      _delay_ms(10);
 		      send_chunck_of_data(contain_both_humidity_tempr,arrange_array);
 		      _delay_ms(100);
-=======
-			      _delay_ms(10);
-			      contain_both_humidity_tempr[0]=0x74;
-			      contain_both_humidity_tempr[1]=0x65;
-			      for(uint8_t i=0;i<2;i++)
-			      {
-				      contain_both_humidity_tempr[2+i]=value_of_tempr[i];
-			      }
-			      itoa(humidity,value_of_humidity,10);
-			      _delay_ms(10);
-			      contain_both_humidity_tempr[4]=0x68;
-			      contain_both_humidity_tempr[5]=0x75;
-			      for(uint8_t i=1;i<5;i++)
-			      {
-				      contain_both_humidity_tempr[5+i]=value_of_humidity[i-1];
-			      }
-		      }
-		      _delay_ms(1300);
-		      reset();
-		      _delay_ms(10);
-		      send_chunck_of_data(contain_both_humidity_tempr,9);
-		      _delay_ms(150);
->>>>>>> c4cac47a7eb1ecc349da7440e02a9c94c31050de
-		      flush_every();
-		      _delay_ms(10);
+			  flush_every();
+			   _delay_ms(10);
+			  arrange_array =0;
+			  getlocation();
+			   _delay_ms(10);
+			   reset();
+			   _delay_ms(10);
+			   send_chunck_of_data(location,26);
+			   _delay_ms(100);
+			   flush_every();
+			   _delay_ms(10);
     }
 }
 void initialize()
@@ -275,7 +231,7 @@ void send_chunck_of_data(char *data_to_send,uint8_t lengths)
 	PORTB|=(1<<SS);
 	_delay_ms(10);
 	PORTB|=(1<<CE);
-    _delay_ms(25);
+    _delay_ms(30);
 	PORTB&=~(1<<CE);
 }
 void reset()
@@ -327,4 +283,71 @@ void flush_every()
 			RWdata(FLUSH_RX);
 			PORTB|=(1<<SS);
 	
+}
+void getlocation()
+{
+	char i=0;
+	char j=1;
+	location[0]=0x67;
+		char takeallvalue=0;
+		while(takeallvalue<=3)
+		{
+			i=return_char();
+			if (i==0x24)//check if variable i is "$"
+			{
+				i=return_char();
+				if (i==0x47)//check if variable i is "G"
+				{
+					i=return_char();
+					if (i==0x50)//check if variable i is "P"
+					{
+						i=return_char();
+						if (i==0x47)//check if variable i is "G"
+						{
+							i=return_char();
+							if (i==0x47)//check if variable i is "G"
+							{
+								i=return_char();
+								if (i==0x41)//check if variable i is "A"
+								{
+									i=return_char();
+									if (i==0x2C)//check if variable i is ","
+									{
+										i=return_char();
+										while(i!=0x2C)
+										{
+											i=return_char();
+										}
+										while (takeallvalue<=3)
+										{
+											i=return_char();
+											if(i==0x2C)
+											{
+												takeallvalue++;
+												if (takeallvalue==2)
+												{
+													location[j]=i;
+													j++;
+												}
+											}
+											else
+											{
+												location[j]=i;
+												j++;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+}
+unsigned char return_char()
+{
+	while (!(UCSRA&(1<<RXC)));//wait until UDR register contain data received from other device
+	return UDR;// return value obtained from other device through Rx pin
 }
